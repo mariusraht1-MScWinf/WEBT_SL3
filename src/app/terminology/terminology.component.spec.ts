b6cf6531b0652f93164d973d3bd9ebad7a33c101
ZdravkaT <zdravka.todorova80@gmail.com>
2021-07-13 15:17:21 +0200

    technology.component angelegt

---
+++ src/app/terminology/terminology.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TerminologyComponent } from './terminology.component';
+
+describe('TerminologyComponent', () => {
+  let component: TerminologyComponent;
+  let fixture: ComponentFixture<TerminologyComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TerminologyComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TerminologyComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/terminology/terminology.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-terminology',
+  templateUrl: './terminology.component.html',
+  styleUrls: ['./terminology.component.css']
+})
+export class TerminologyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
