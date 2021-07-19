043947789eaa9daa7e4813974032edea22836a4d
ZdravkaT <zdravka.todorova80@gmail.com>
2021-07-19 10:48:38 +0200

    impost component app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TerminologyAddComponent } from './terminology-add/terminology-add.component';
 
 const routes: Routes = [];
 
